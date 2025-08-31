f2e7cb3196554b760ea15708bdb847200f92c9cd
01TGMA <Ghostmanhades@gmail.com>
2025-08-31 23:17:29 +0100

    Imported PopUp into javascript

--- Script.js
+++ Script.js
@@ -1,6 +1,7 @@
 const learnMore = document.getElementById("Learn")
 const sales = document.getElementById("Sales")
 const support = document.getElementById("Support")
+const pop = document.getElementById("PopUp")
 
 learnMore.addEventListener("click", () => {
     window.location.href = "About.html"
