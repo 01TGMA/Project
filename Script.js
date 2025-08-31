297aed9879c2c19404fa967298525551c1537167
01TGMA <Ghostmanhades@gmail.com>
2025-08-31 23:14:32 +0100

    imported Sales and support buttons into javascript file

--- Script.js
+++ Script.js
@@ -1,5 +1,7 @@
 const learnMore = document.getElementById("Learn")
+const sales = document.getElementById("Sales")
+const support = document.getElementById("Support")
 
 learnMore.addEventListener("click", () => {
     window.location.href = "About.html"
-})
+}) 
