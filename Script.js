f0a1904ad9b0dbdb1363f0338a145d2117643577
01TGMA <Ghostmanhades@gmail.com>
2025-09-03 00:12:07 +0100

    Corrected mistake

--- Script.js
+++ Script.js
@@ -1,5 +1,5 @@
 const learnMore = document.getElementById("Learn")
-const sales = document.getElementById("Sales")
+const  = document.getElementById("Sales")
 const support = document.getElementById("Support")
 const pop = document.getElementById("PopUp")
 
@@ -7,7 +7,7 @@ learnMore.addEventListener("click", () => {
     window.location.href = "About.html"
 }) 
 
-support.addEventListener("click", () => {
+sales.addEventListener("click", () => {
     document.getElementById("PopUp").style.display = "flex";
 })
 
